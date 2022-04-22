c92ec7391a17bbdda25803410b86f6af96cd62b1
Théo Silva | 0x0th30 <thedorotheodore@gmail.com>
2022-04-21 22:16:57 -0300

    Finished API coding

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	new Date;
+	const currentTimestamp = Date.now()
+	console.log(`new request received at ${currentTimestamp}`);
+	return res.json({ timestamp: currentTimestamp });
+});
+
+const PORT = 3333;
+app.listen(PORT, () => {
+	console.log(`listenning at http://localhost:${PORT}`);
+});
